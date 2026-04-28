9a236bcac6cd66b47cfe35537f2ff502dd39f631
MakiWinster <makiiiiiiiho@gmail.com>
2026-04-28 22:08:11 +0800

    style: 胶囊样式

--- frontend/src/components/MobileCapsule.vue
+++ frontend/src/components/MobileCapsule.vue
@@ -16,11 +16,11 @@
               viewBox="0 0 24 24"
               fill="none"
               stroke="currentColor"
-              stroke-width="2"
+              stroke-width="1.6"
               stroke-linecap="round"
               stroke-linejoin="round"
             >
-              <path d="M4 6h16M4 12h16M4 18h16" />
+              <path d="M5 7h14M5 12h14M5 17h14" />
             </svg>
           </span>
         </div>
